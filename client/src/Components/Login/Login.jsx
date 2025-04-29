9756a3211ef569d9f88f681c2e86075dc97ab109
mobin mathew <mobinmathew888@gmail.com>
2025-04-29 12:19:59 +0530

    fix bug

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './login.css';
+import './Login.css';
 import { AuthContext } from '../context/AuthContext';
 
 function Login() {
